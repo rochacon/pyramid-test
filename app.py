fadc3c97c873429fe209fa698990c9db11a4c71d
Rodrigo Chacon <rochacon@gmail.com>
2013-05-07 11:38:17 -0300

    Fooling around

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 # coding: utf-8
+import json
+
+from bson.objectid import ObjectId
+from pymongo import MongoClient
 from pyramid.config import Configurator
 from pyramid.response import Response
+
+
+mongo = MongoClient()
+db = mongo.tumblelog
+
+
+def get_obj(request, _id):
+    obj = db.objs.find_one({'_id': ObjectId(_id)})
+    if obj:
+        return Response(json.dumps(obj))
+    return Response(status=404)
+
+
+def put_obj(request):
+    id = db.objs.insert(request.json)
+    if id:
+        return Response(json.dumps({'success': True, 'id': str(id)}), status=201)
+    return Response(json.dumps({'error': 'Not created'}), status=500)
 
 
 def health(request):
@@ -8,10 +30,19 @@ def health(request):
 
 
 # URLs
-c = Configurator()
-c.add_route('health', '/health')
-c.add_view(health, route_name='health')
+config = Configurator()
+config.add_route('health', '/health')
+config.add_view(health, route_name='health')
+config.add_route('get', '/get', request_method=('get',))
+config.add_view(get_obj, route_name='get')
+config.add_route('put', '/put', request_method=('PUT',))
+config.add_view(put_obj, route_name='put')
 
 # health app
-application = c.make_wsgi_app()
+application = config.make_wsgi_app()
 
+
+if __name__ == '__main__':
+    import wsgiref.simple_server
+    server = wsgiref.simple_server.make_server('127.0.0.1', 8000, application)
+    server.serve_forever()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
 # coding: utf-8
+from pymongo import MongoClient
 from pyramid.testing import DummyRequest as Request
 
-from app import health
+from app import db, get_obj, health, put_obj
 
 
 def test_health_page():
@@ -10,3 +11,37 @@ def test_health_page():
     assert response.status_code == 200
     assert response.body == "I'm alive!"
 
+
+def test_insert_obj():
+    db.objs.drop()
+
+    request = Request()
+    request.json = {"test": "OK"}
+    response = put_obj(request)
+    assert response.status_code == 201
+    assert db.objs.count() == 1
+    assert response.json == {'success': True, 'id': str(db.objs.find()[0]['_id'])}
+
+
+def test_insert_failure():
+    pass
+    # return Response(json.dumps({'error': 'Not created'}), status=500)
+
+
+def test_get_obj():
+    db.objs.drop()
+    _id = db.objs.insert({'test': True})
+
+    request = Request()
+    response = get_obj(request, str(_id))
+    assert response.status_code == 200
+    assert response.json == {'test': True}
+
+
+def test_get_obj_not_found():
+    db.objs.drop()
+
+    request = Request()
+    response = get_obj(request, '0' * 24)
+    assert response.status_code == 404
+    assert response.body == ''
